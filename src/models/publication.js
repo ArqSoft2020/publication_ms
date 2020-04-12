d1c59c6e7849af5c967b3bc298b2d6783692328f
Miller <macorreag@unal.edu.co>
2020-04-12 11:21:11 -0500

    se añade modelo para mongoose[mongoose not install] + descriptionAPI.yml

---
+++ src/models/publication.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const publicationSchema = new Schema ({
+    title : {
+      type: String
+    } ,
+    description : String,
+    state_publication : Boolean,
+    contact_information : String,
+    id_image : {
+        type : ObjectId,
+        default : 0
+    } ,
+    stock : {
+        type :Number,
+        default : 0
+    } ,
+    expiration_date : {
+        type: Date,
+        default : Date.now
+    } ,
+    price : {
+        type :  Number,
+        default : 0
+    },
+    categories : [String]
+})
